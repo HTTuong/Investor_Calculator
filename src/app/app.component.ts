9e04fba3c228cb4cf771a89fbdbdb36162f779d4
Tuong Hoang <httuong.2705@gmail.com>
2025-08-17 19:48:36 +0300

    SW-5: Add investment calculation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import type { InvestmentData } from './investment-input.model';
+
+export interface AnnualData {
+  year: number;
+  interest: number;
+  valueEndOfYear: number;
+  annualInvestment: number;
+  totalInterest: number;
+  totalAmountInvested: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,4 +18,35 @@ import { UserInputComponent } from './user-input/user-input.component';
   standalone: true,
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  result: AnnualData[] = [];
+
+  onCalculateInvestmentResults(data: InvestmentData) {
+    const { initialInvestment, annualInvestment, expectedReturn, duration } =
+      data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+
+  calculateInvestment(data: InvestmentData) {
+    this.result = this.onCalculateInvestmentResults(data);
+  }
+}
